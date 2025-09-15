8d88b4a9527aad49fa9cfb59e30b59adb215330a
FAZLIDDIN <fazliddinauu@gmail.com>
2025-09-15 10:43:40 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/": "http://localhost:3000",
+      "/api/": {
+        target: "https://cinema-cu8z.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/uploads/": {
+        target: "https://cinema-cu8z.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
